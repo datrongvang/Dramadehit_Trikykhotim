6de0cbb33742d297da747bea798a725fdb38e40c
nguyễn minh đạt <71458580+datrongvang@users.noreply.github.com>
2023-04-20 10:45:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8/3 của Nhi';
+const titleCard = 'Tặng Nhi';
+const contentCard = 'Chúc Nhi một ngày 8/3 thật may mắn và xinh đẹp! Gặp được e là điều may mắn a nhận được trong năm nay. Cầu chúc những điều tốt đẹp luôn bên e. Ko Thương e nữa :))!!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
