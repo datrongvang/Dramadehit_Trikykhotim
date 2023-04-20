1aee1d6da3dedce9ec613039f9758696e333e45d
nguyễn minh đạt <71458580+datrongvang@users.noreply.github.com>
2023-04-20 11:00:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const nameGirl = '';
 const giftUrl = '';
-const eventName = 'Chúc Mừng 8/3 của Nhi';
-const titleCard = 'Tặng Nhi';
-const contentCard = 'Chúc Nhi một ngày 8/3 thật may mắn và xinh đẹp! Gặp được e là điều may mắn a nhận được trong năm nay. Cầu chúc những điều tốt đẹp luôn bên e. Ko Thương e nữa :))!!';
+const eventName = 'DRAMA RÚP';
+const titleCard = 'MÃI ĐỈNH';
+const contentCard = ' CHÚC AE LÀM VIỆC VUI VẺ, HÃY CHỌN CHO MÌNH MỘT LỐI ĐI VÀ CỐ GẮNG THEO ĐUỔI, HÃY LẮNG NGHE CON TIM, THÀNH CÔNG LÀ CÂU TRẢ LỜI HAY NHẤT:))!!';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
